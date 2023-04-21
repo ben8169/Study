693de7b4fadccafccb290a398827e70977a6a8e1
ben8169 <48664269+ben8169@users.noreply.github.com>
2023-04-22 00:10:12 +0900

    Update 12강.py

--- 12강 모듈/12강.py
+++ 12강 모듈/12강.py
@@ -1,41 +1,39 @@
 # 실전문제
 # 12-1 모듈 작성해보기
 
-# my_math.py
-
-# def add(lst):
-#     print(sum(lst))
-#     return
-
-
-# def subtract(lst):
-#     if len(lst) == 1:
-#         print(lst[0])
-#         return
-#     elif len(lst) == 0:
-#         print("null")
-#         return
-#     else:
-#         lst[1:] = map(lambda x: -1*x, lst[1:])
-#         print(list(lst))
-#         print(sum(lst))
-#         return
+#my.math.py
+def add(lst):
+    print(sum(lst))
+    return
 
 
-# def multiply(lst):
-#     identity = 1
-#     for i in lst:
-#         identity *= i
-#     print(identity)
+def subtract(lst):
+    if len(lst) == 1:
+        print(lst[0])
+        return
+    elif len(lst) == 0:
+        print("null")
+        return
+    else:
+        lst[1:] = map(lambda x: -1*x, lst[1:])
+        print(sum(lst))
+        return
 
 
-# def divide(lst):
-#     pivot = lst[0]
-#     for i in lst[1:]:
-#         pivot /= i
-#     print(pivot)
+def multiply(lst):
+    identity = 1
+    for i in lst:
+        identity *= i
+    print(identity)
 
 
+def divide(lst):
+    pivot = lst[0]
+    for i in lst[1:]:
+        pivot /= i
+    print(pivot)
+
+#main.py
 import my_math as mm
 
 while True:
@@ -44,11 +42,25 @@ while True:
         print("연산자 입력오류")
         continue
 
-    num_list = list(map(int, input().split()))
+    num_list = list(map(int, input("숫자 리스트를 띄어쓰기로 입력하세요").split()))
+    if len(num_list) == 0:
+        print('숫자가 입력되지 않았습니다. 다시 시작')
+        continue
 
     if operator == '+':
+        mm.add(num_list)
+    elif operator == '-':
+        mm.subtract(num_list)
+    elif operator == '*':
+        mm.multiply(num_list)
+    elif operator == '/':
+        mm.divide(num_list)
 
-        # mm.add()
-        # mm.subtract()
-        # mm.multiply()
-        # mm.divide()
+    print("="*30)
+
+    if input("계속할까요?(Y/N)").lower() == 'n':
+        print('종료')
+        break
+    else:
+        print("="*30)
+        
