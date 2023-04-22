80072b26a4c2bf6ac5df108a63a4045633c07463
ben8169 <48664269+ben8169@users.noreply.github.com>
2023-04-22 23:54:51 +0900

    Update 12강.py

--- 12강 모듈/12강.py
+++ 12강 모듈/12강.py
@@ -64,3 +64,36 @@ while True:
     else:
         print("="*30)
         
+#12-2 matplotlib 사용하기 
+import matplotlib.pyplot as plt
+def sorting(list):
+    while True:
+        ascend_or_descend = int(input("1.오름차순 2.내림차순 숫자를 입력하세요."))
+        if ascend_or_descend == 1:
+            list.sort()
+            return list
+        elif ascend_or_descend == 2:
+            list.sort(reverse=True)
+            return list
+        else:
+            print("잘못 입력하셨습니다.")
+
+def axis(list):
+    while True:
+        axis = int(input("1.세로 막대그래프 2.가로 막대그래프 숫자를 입력하세요."))
+        if axis == 1:
+            plt.bar(range(1,len(list)+1),list)
+        elif axis == 2:
+            plt.barh(range(1,len(list)+1),list)
+        else:
+            print("잘못 입력하셨습니다.")
+            continue
+        plt.show()
+        return
+
+num_list = list(map(int,input("숫자 리스트 입력 : ").split()))
+sorted_list = sorting(num_list)
+axis(sorted_list)
+
+       
+        
