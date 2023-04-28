0d31816a5f0320c8f47ef6f9bdffe40e2eec8765
ben8169 <48664269+ben8169@users.noreply.github.com>
2023-04-28 23:37:12 +0900

    Update 14강.py

--- 14장_파일 입출력/14강.py
+++ 14장_파일 입출력/14강.py
@@ -6,3 +6,23 @@ with open('1.txt', 'r') as f1, open('2.txt', 'r') as f2:
     else:
         print('complete')
 
+
+# 14-2 한 줄씩 입력하여 txt 생성
+f = open('write_by_lines.txt', 'w')
+text_sum = ''
+
+while True:
+    text = input("죽기 전에 하고 싶은 일은?:")
+    text_sum += text + '\n'
+    ask_continue = input("계속 입력?(Y/N)")
+    if ask_continue.lower() == 'n':
+        break
+    else:
+        continue
+
+f.writelines(text_sum)
+f.close()
+
+txt_file = open('write_by_lines.txt', 'r')
+print(txt_file.read())
+txt_file.close()
