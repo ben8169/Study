ea0b4bf7f1ce594d98be7a6aff8fe502a57196cc
ben8169 <48664269+ben8169@users.noreply.github.com>
2023-05-16 00:33:37 +0900

    Update 1015_수열 정렬.py

--- Algorithm/백준/Silver5/1015_수열 정렬.py
+++ Algorithm/백준/Silver5/1015_수열 정렬.py
@@ -1,3 +1,4 @@
+#sol1
 N = int(input())
 lst = [0]*N
 arr = list(map(int, input().split()))
@@ -7,7 +8,15 @@ for i in range(limit+1):
     for j in arr:
         if i == j:
             lst[arr.index(j)] = cnt
+            arr[arr.index(j)] = -1
             cnt += 1
-            print(lst)
 
 print(*lst)
+
+#sol2
+import sys
+N = int(sys.stdin.readline())
+lst = [0]*N
+arr = list(map(int, sys.stdin.readline().split()))
+s=sorted(arr,key=int)
+for i in arr:print(x:=s.index(i));s[x]=-1
