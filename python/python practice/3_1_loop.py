473903becc4abc224a17d7ed37c133392a756aa3
ben8169 <ben8169@outlook.kr>
2023-06-15 22:52:55 +0900

    Update 3_1_loop.py

--- python/python practice/3_1_loop.py
+++ python/python practice/3_1_loop.py
@@ -23,3 +23,42 @@
 # str = '----'
 # for i in range(4):
 #     print(str.replace('-', '*', i+1))
+
+
+#   0123
+# 3 ****
+# 2 ***-
+# 1 **--
+# 0 *---
+
+# sol1
+# for i in range(4):
+#     print('*'*(4-i)+'-'*i)
+
+# sol2
+# for i in range(4-1, -1, -1):
+#     for j in range(4):
+#         if i >= j:
+#             print('*', end='')
+#         else:
+#             print('-', end='')
+#     print("")
+
+
+# 0 ---*
+# 1 --**
+# 2 -***
+# 3 ****
+
+#sol1
+# for i in range(4):
+#     print('-'*(3-i)+'*'*(i+1))
+
+# ****
+#  ***
+#   **
+#    *
+
+
+# for i in range(4):
+#     print(' ' * i + '*' * (4-i))
