39a9061aa5b7b26e1d428cc658b62d10c3ec0bb9
ben8169 <ben8169@outlook.kr>
2023-08-15 19:20:07 +0900

    Create basic_usage.c

---
+++ C/basic_usage.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#define TEXT  "string도 선언 가능."
+
+int main (void){
+    // // printf 안에 여러 "" 들어갈 수 있다.
+    // printf("h""e""l""l""o""\n");
+    // printf(TEXT"\n");
+
+    // // 이스케이프 시퀀스
+    // printf("\' \" \? \\");
+    // printf("\a \b \n");
+
+    // // 서식지정자
+    // printf("%c\n",'a');
+    // printf("%s\n",TEXT);
+    // printf("%d\n",-420000000);
+    // printf("%i\n",-420000000);
+    // long n1 = -420000200;
+    // printf("%ld\n",n1);
+    // long long n2 = -23245342532500000;
+    // printf("%lld\n",n2);
+    // printf("%u\n",420000000);
+    // printf("%f\n",1.23456789);
+    // printf("%.10f\n",1.23456789);
+    // printf("%e\n",1.23456789);
+    // printf("%E\n",1.23456789);
+    // printf("%o\n",10);
+    // printf("%x\n",1234);
+    // printf("%X\n",1234);
+    // printf("%d%%\n",100);
+
+
+    
+
+    
+    return 0;
+}
+
+/*  전처리기
+컴파일러
+어셈블러
+링커
+*/
