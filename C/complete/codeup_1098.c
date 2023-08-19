f21c4a9397c1a8a265e01238b0404c46594d6af7
ben8169 <ben8169@outlook.kr>
2023-08-20 00:09:51 +0900

    Create codeup_1098.c

---
+++ C/complete/codeup_1098.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+int main(void){
+    unsigned char h,w;
+    scanf("%d %d",&h,&w);
+    int arr[h][w];
+
+    unsigned char n=0,h=0,;
+    scanf("%d",&n);
+
+// ========
+
+
+
+
+
+
+    return 0;
+}
