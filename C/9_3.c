53a278945a4e0846026d651ff971d895a9bd5f24
ben8169 <ben8169@outlook.kr>
2023-09-07 21:45:40 +0900

    Update 9_3.c

--- C/9_3.c
+++ C/9_3.c
@@ -2,8 +2,27 @@
 
 int main()
 {
+z// 	// 1. 이런 식이면 어떻게든 arr길이는 i로 알게 된다. 왜? 인덱스가 꼭 필요하거든
+//   static int arr[] = {}; // int *arr = NULL
 
-	return 0;
+//   int i = 0, n =0 ;
+//   while(1){
+//     scanf("%d",&n);
+//     if (n == -3)
+//       break;
+//     *(arr+i) = n;
+//     i++;
+//   }
+
+//   printf("[");
+//   for (int j=0; j<i; j++){
+//   	printf("%d ",arr[j]);
+//   }
+//    printf("]");
+//   return 0;
+
+
+	// 2. 동적할당도 똑같은데? 결국 malloc 할 만큼의 i 를 알고 있다는 가정하에 모든 할당이 이뤄지니깐요
 }
 
 
