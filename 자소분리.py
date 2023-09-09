039bbd1d9d6bf46e5ec4376611d3bf5f6e753bbb
ben8169 <ben8169@outlook.kr>
2023-09-10 00:14:13 +0900

    Update 자소분리.py

--- 자소분리.py
+++ 자소분리.py
@@ -5,7 +5,6 @@ import argparse
 
 def change_nfc_all_dir(dirname):
     filenames = os.listdir(dirname)
-    print(filenames)
     for filename in filenames:
         before_filename = os.path.join(dirname, filename)
         after_filename = normalize('NFC', before_filename)
@@ -17,14 +16,16 @@ def change_nfc_all_dir(dirname):
 
 def main():
     parser = argparse.ArgumentParser(description='자소분리')
-    parser.add_argument('--path', type=str, default="C:\\Users\\ben81\\iCloudDrive", help='path')
-    parser.add_argument('--num', type=int, default=10, help='numbers of repeat')
+    parser.add_argument('-p','--path', type=str, default="C:\\Users\\ben81\\iCloudDrive", help='path')
+    parser.add_argument('-r','--rep', type=int, default=10, help='numbers of repeat')
     args = parser.parse_args()
 
-    for i in range(args.num):
+    print(f"current path = {args.path}"}")
+    for i in range(args.rep):
+        print(f"{i+1}th start")
         try:
-            change_nfc_all_dir(args.p)
-            print(f"{i}th complete")
+            change_nfc_all_dir(args.path)
+            print(f"{i+1}th complete")
         except:
             pass
 
