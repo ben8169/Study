87fbfb5eacaee71feb2a1737043291a46753cbe7
ben8169 <ben8169@outlook.kr>
2023-10-25 19:38:27 +0900

    edit fuelEfficiency.c

--- C/assignment/fuelEfficiency.c
+++ C/assignment/fuelEfficiency.c
@@ -8,10 +8,11 @@ int main(void){
     do{
         printf("주입연료량(리터), 주행거리(km)입력:");
         scanf("%lf %lf",&liter,&km);
-        printf("1리터로 주행할 수 있는 거리 = %lfkm",FuelEfficiency(liter,km));
+        printf("1리터로 주행할 수 있는 거리 = %lfkm\n",FuelEfficiency(liter,km));
         printf("다시 계산하겠습니까?(Y/N)");
-        scanf("%c",&check);
-    } while(check != 'Y' || check != 'y');
+        scanf(" %c",&check);
+        getchar();
+    } while(check == 'Y' || check == 'y');
     printf("프로그램 종료\n");
     return 0;
     }
