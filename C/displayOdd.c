e0b4884dfd4ab15e7d5c2e7062bbcd9728999df6
ben8169 <ben8169@outlook.kr>
2023-10-25 19:08:25 +0900

    add shape.c , displayOdd.c

---
+++ C/displayOdd.c
@@ -0,0 +1,31 @@
+// 2022313356 김지헌
+#include <stdio.h>
+
+void DisplayOdd(int s, int e, int m);
+
+int main(void){
+    int s, e, m;
+    scanf("%d %d %d",&s, &e, &m);
+    DisplayOdd(s,e,m);
+    return 0;
+}
+
+
+void DisplayOdd(int s, int e, int m){
+    int cnt = 0;
+    for (int i=s;i<e;i+=2){
+        if (cnt > 10){
+            printf("\n");
+            cnt = 0;
+            }
+        if ((i%2)==0)
+            i += 1;
+
+        if ((i%m) != 0){
+            printf("%d ",i);
+            cnt += 1;
+        }
+    }
+
+
+}
