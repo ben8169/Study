60f7e2e13e72ef08cbbdc46b7d9e51f5bbb68213
ben8169 <ben8169@outlook.kr>
2023-10-26 23:39:42 +0900

    start 5032.c

---
+++ C/3052.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+int main(void){
+    int arr[10]={0}, arr2[10]={0};
+    for (int i=0;i<10;i++){
+        int n=0;
+        scanf("%d",&n);
+        arr[i]=n%42;
+    }
+
+    arr2[0] = arr[0];
+
+    for (int i=0;i<10;i++){
+
+
+    }
+
+
+
+
+    return 0;
+}
