b5a2e8f1650e3c687fa65a98a5692b338b413436
ben8169 <ben8169@outlook.kr>
2023-11-04 23:30:38 +0900

    complete struct

--- C/struct.c
+++ C/struct.c
@@ -11,15 +11,35 @@ typedef struct{
 	int grade;
 }student;
 
+typedef struct{
+	int a : 31;
+	int b : 1;
+	int c : 1;
+}s1;
+
+typedef struct{
+	int a;
+	int b;
+	int c;
+}s2;
 
 
 int main(void){
-	student kim = {{"kim","jiheon"}, 1};
-	student *ptr = &kim;
-//	name *n_ptr = &(ptr->student_name);
-	name *n_ptr = &((&kim)->student_name);
-//	printf("%s %s %d\n",ptr->student_name.first,ptr->student_name.last, ptr->grade);
-	printf("%s %s %d\n",n_ptr->first,n_ptr->last, ptr->grade);
+//  // struct reference
+// 	student kim = {{"kim","jiheon"}, 1};
+// 	student *ptr = &kim;
+// //	name *n_ptr = &(ptr->student_name);
+// 	name *n_ptr = &((&kim)->student_name);
+// //	printf("%s %s %d\n",ptr->student_name.first,ptr->student_name.last, ptr->grade);
+// 	printf("%s %s %d\n",n_ptr->first,n_ptr->last, ptr->grade);
+// 	return 0;
+
+// //struct size
+// printf("%d\n",sizeof(s1));
+// printf("%d\n",sizeof(s2));
+
+	enum Days {MON,TUE,WED};
+	enum Days day = 3;
+	printf("%d",day);
 	return 0;
 }
-
