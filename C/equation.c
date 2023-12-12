45d2abd207f781d94495517f722b9f508607dd6b
ben8169 <ben8169@outlook.kr>
2023-12-12 23:37:52 +0900

    Add annotation at equation.c

--- C/equation.c
+++ C/equation.c
@@ -17,15 +17,20 @@ void root(struct equation *p) {
 }
 
 int main(void) {
-  struct equation eq[5];
-  struct equation *p = &eq[0];
-  int n = 5;
+  struct equation eq[100];
+  struct equation *p = &eq[0]; 
+  /* p는 eq라는 리스트 중 한 덩어리의 struct 를 가리킴 --> 그런데 (p = eq; p < eq + n; p++)로 연산할 때는 위치는 상관없으나, (int i = 0; i < n; i++)로 연산하려 할 경우 p가 eq[0]을 가리키게 하는 초기항이 없으므로 처음부터 p가 eq[0]을 가리켜야 함 
+  */
 
+  int n;
+  scanf("%d",&n);
 
   for (int i = 0; i < n; i++) {
     scanf("%d %d %d", &eq[i].a, &eq[i].b, &eq[i].c);
   }
 
+  // // 1. p로 연산
+  // // p연산시에 n을 단순 반복횟수로 해도 괜춘(ptr연산이므로 알아서 struct size만큼 커짐)
   // for (p = eq; p < eq + n; p++) {
   //   root(p);
   // }
@@ -33,6 +38,7 @@ int main(void) {
   //   printf("a = %d, b = %d, c = %d, %s\n", p->a, p->b, p->c, p->det);
   // }
   
+  //2. i로 연산
   for (int i = 0; i < n; i++) {
     root(p + i);
   }
