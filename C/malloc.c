7555eefde01e6eeba18535643e43c840a47a175d
ben8169 <ben8169@outlook.kr>
2023-12-21 20:51:07 +0900

    modify malloc.c

--- C/malloc.c
+++ C/malloc.c
@@ -3,10 +3,11 @@
 
 int main(void){
     int num = 10, total_num = 30;
-    int* malloc_arr, calloc_arr, realloc_arr;
+    int* malloc_arr, calloc_arr, realloc_arr, realloc_NULL;
     malloc_arr = (int*) malloc(num * sizeof(int));
     calloc_arr = (int*) calloc(num, sizeof(int));
     realloc_arr = (int*) realloc(malloc_arr, total_num * sizeof(int));
 
+    realloc_NULL = (int*) realloc(NULL,total_num * sizeof(int));
     return 0;
 }
