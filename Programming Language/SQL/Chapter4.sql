885d94a009f358e3847d18c1b8abaffe2d15db5d
ben8169 <ben8169@outlook.kr>
2024-01-23 11:48:38 +0900

    Complete Chapter4.sql

--- Programming Language/SQL/Chapter4.sql
+++ Programming Language/SQL/Chapter4.sql
@@ -87,9 +87,239 @@ SELECT DISTINCT(M.mem_id), M.mem_name, M.addr
         
 # 외부 조인 -> 한곳이라도 있으면 조인할때
 SELECT M.mem_id, M.mem_name, B.prod_name, M.addr
-	FROM buy B
-		INNER JOIN member M
-        ON B.mem_id = M.mem_id
+	FROM member M
+        RIGHT OUTER JOIN buy B
+        ON M.mem_id = B.mem_id
 	ORDER BY M.mem_id;
     
+-- SELECT M.mem_id, M.mem_name, B.prod_name, M.addr
+-- 	FROM member M
+--         LEFT OUTER JOIN buy B
+--         ON M.mem_id = B.mem_id
+-- 	ORDER BY M.mem_id;
+
+#  가입만 하고 한번도 구매한 적 없는 회원 목록 추출
+# 1 여기서 buy가 왼쪽이 되어야, 구매 내역이 NULL인 친구도 포함해서 전부 나옴.
+SELECT M.mem_id, M.mem_name, M.addr, concat(M.phone1,M.phone2) "phone", B.prod_name
+	FROM member M
+		LEFT JOIN buy B
+        ON M.mem_id = B.mem_id
+	WHERE B.prod_name IS NULL
+    ORDER BY M.mem_id;
     
+#2 다음 두 쿼리는 같다.
+-- SELECT * 
+-- 	FROM A
+--     LEFT JOIN B
+--     
+-- SELECT *
+-- 	FROM B
+--     RIGHT JOIN A
+
+#3 FULL OUTER JOIN 이란 것이 존재한다.
+
+# 기타 조인
+## 상호 조인 (cross join) --ON 사용 불가능, 결과 내용은 의미없고, 주로 대용량 데이터 생성할 때 사용
+SELECT *
+	FROM buy
+		CROSS JOIN member;
+    
+CREATE TABLE cross_table
+	SELECT *
+		FROM sakila.actor
+			CROSS JOIN world.country;
+            
+SELECT * FROM cross_table LIMIT 10;
+
+## 자체 조인(self join) -- 여러 col에 같은 데이터
+USE market_db;
+CREATE TABLE emp_table (emp CHAR(4), manager CHAR(4), phone VARCHAR(8));
+
+INSERT INTO emp_table VALUES('대표', NULL, '0000');
+INSERT INTO emp_table VALUES( '영업이사' ,'대표'  ,'1111' );
+INSERT INTO emp_table VALUES(  '관리이사', '대표' ,'2222' );
+INSERT INTO emp_table VALUES( '정보이사' ,'대표'  , '3333');
+INSERT INTO emp_table VALUES( '영업과장' ,'영업이사'  ,'1111-1 ');
+INSERT INTO emp_table VALUES( '경리부장' , '관리이사' , '2222-1');
+INSERT INTO emp_table VALUES(  '인사부장', '관리이사' , '2222-2');
+INSERT INTO emp_table VALUES( '개발팀장' , '정보이사' , '3333-1');
+INSERT INTO emp_table VALUES( '개발주임' , '정보이사' , '3333-1-1');
+
+
+SELECT A.emp "직원",  B.emp "직속상관", B.phone "직속상관연락처"
+	FROM emp_table A
+		INNER JOIN emp_table B
+        ON A.manager = B.emp
+	WHERE A.emp = '경리부장';
+
+# 04-3 SQL 프로그래밍
+-- DROP PROCEDURE IF EXISTS ifProc1;
+-- DELIMITER $$
+-- CREATE PROCEDURE ifProc1()
+-- BEGIN
+-- 	IF 100 = 100 THEN
+-- 		SELECT '100은 100과 같습니다'; -- select + 문자열은 print와 비슷한 기능
+-- 	END IF;
+--     END $$
+--     DELIMITER ;
+--     CALL ifProc1;
+    
+    
+
+DROP PROCEDURE IF EXISTS ifProc2();
+DELIMITER $$
+CREATE PROCEDURE ifProc2()
+BEGIN 
+	DECLARE myNum INT;
+    SET myNum = 200;
+    IF myNum = 100 THEN
+		SELECT '100입니다';
+	ELSE
+		SELECT '100이 아닙니다';
+	END IF;
+END $$
+DELIMITER ;
+CALL ifProc2();
+
+
+DROP PROCEDURE IF EXISTS ifProc3();
+DELIMITER $$
+CREATE PROCEDURE ifProc3()
+BEGIN
+	DECLARE debutDate DATE;
+    DECLARE curDate DATE;
+    DECLARE days INT;
+    
+    SELECT debut_date INTO debutDate
+		FROM market_db.member
+        WHERE mem_id = 'APN';
+        
+	SET curDATE = current_date();
+    SET days = DATEDIFF(curDATE, debutDate);
+    
+    IF (days/365) >= 5 THEN
+		SELECT CONCAT('데뷔한 지', days, '일이나 지났습니다. 핑순이들 축하합니다!');
+	ELSE
+		SELECT '데뷔한 지 ' + days + '일밖에 안되엇네요. 핑순이들 화이팅~';
+	END IF
+END $$
+DELIMITER ;
+CALL ifProc3();
+
+
+DROP PROCEDURE IF EXISTS caseProc();
+DELIMITER $$
+CREATE PROCEDURE caseProc()
+BEGIN
+	DECLARE point INT;
+    DECLARE credit CHAR(1);
+    SET point = 88;
+    
+    CASE 
+		WHEN point >= 90 THEN
+			SET credit = 'A';
+		WHEN point >= 80 THEN
+			SET credit = 'B';
+		WHEN point >= 70 THEN
+			SET credit = 'C';
+		WHEN point >= 60 THEN
+			SET credit = 'D';
+		ELSE
+			SET credit = 'F';
+		END CASE;
+        SELECT CONCAT('취득 점수==>', point), concat('학점==>',credit);
+	END $$
+    DELIMITER ;
+    CALL caseProc();
+        
+#실전
+SELECT mem_id, SUM(price*amount) "총구매액"
+	FROM buy
+	GROUP BY mem_id
+   ORDER BY 총구매액 DESC;
+   
+SELECT B.mem_id, M.mem_name, SUM(price*amount) "총구매액",
+	CASE
+		WHEN (SUM(price*amount)>=1500) THEN '최우수고객'
+        WHEN (SUM(price*amount)>=1000) THEN '우수고객'
+        WHEN (SUM(price*amount)>=1) THEN '일반고객'
+        ELSE '유령고객'
+	END "회원등급"
+	FROM buy B
+		RIGHT JOIN member M
+        ON B.mem_id = M.mem_id
+	GROUP BY M.mem_id  -- 구매 테이블에는 4명만 존재하므로, 더 넓은 M.mem_id 사용해야 함
+    ORDER BY 총구매액 DESC;
+    
+
+DROP PROCEDURE IF EXISTS whileProc();
+DELIMITER $$
+CREATE PROCEDURE whileProc()
+BEGIN
+	DECLARE i INT;
+    DECLARE hap INT;
+    SET i = 1;
+    SET hap = 0;
+    
+    WHILE (i<=100) DO
+		SET hap = hap + i;
+        SET i = i + 1;
+	END WHILE;
+    SELECT '1부터 100까지의 합 ==>' , hap;
+END $$
+DELIMITER ;
+CALL whileProc();
+
+
+DROP PROCEDURE IF EXISTS whileProc2();
+DELIMITER $$
+CREATE PROCEDURE whileProc2()
+BEGIN
+	DECLARE i INT;
+    DECLARE hap INT;
+    SET i = 1;
+    SET hap = 0;
+    
+    myWhile:
+    WHILE (i<=100) DO
+		IF (i%4 = 0) THEN
+			SET i = i+1;
+            ITERATE myWhile;
+		END IF;
+        SET hap = hap + i;
+        IF (hap>1000) THEN
+			LEAVE myWhile;
+		END IF;
+        SET i = i+1;
+	END WHILE;
+    
+    SELECT '4의 배수를 제외하고 1부터 100까지의 합, 1000 넘으면 종료 ==>'  + hap;
+END $$
+DELIMITER ;
+CALL whileProc2();
+
+
+
+#동적 SQL => PREPARE & EXECUTE
+
+
+use market_db;
+PREPARE myQuery FROM 'SELECT * FROM member WHERE mem_id = "BLK"';
+EXECUTE myQuery;
+DEALLOCATE PREPARE myQuery;
+
+
+
+DROP TABLE IF EXISTS gate_table;
+CREATE TABLE gate_table (id INT AUTO_INCREMENT PRIMARY KEY, entry_time DATETIME);
+
+SET @curDate = current_timestamp();
+SET @curDate2= current_timestamp();
+
+PREPARE myQuery FROM 'INSERT INTO gate_table VALUES(NULL, ?)' ;
+EXECUTE myQuery USING @curDate;
+EXECUTE myQuery USING @curDate2;
+DEALLOCATE PREPARE myQuery;
+
+SELECT * FROM gate_table;
+
