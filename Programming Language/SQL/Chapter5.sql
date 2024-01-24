22253b014e35f27e4113f3f4c2c849829a3d5aa4
ben8169 <ben8169@outlook.kr>
2024-01-25 02:17:31 +0900

    Complete SQL_Chapter5

--- Programming Language/SQL/Chapter5.sql
+++ Programming Language/SQL/Chapter5.sql
@@ -120,14 +120,49 @@ SELECT * FROM member;
 # 05-3 VIEW
 # 1. 보안에 도움이 된다.
 # 2. 복잡한 sql을 단순하게 저장해 놓고 쓸 수 있다.
-CREATE VIEW v_member
+USE market_db;
+CREATE VIEW v_viewtest1
 AS 
+	SELECT B.mem_id AS "Member ID", M.mem_name AS "Member Name" , B.prod_name AS "Product Name", CONCAT (M.phone1, M.phone2) AS "Office Phone"
+    FROM buy B
+		JOIN member M
+        ON B.mem_id = M.mem_id;
+SELECT DISTINCT `Member ID`, `Member Name` FROM v_viewtest1; -- 백틱을 사용
+
+DROP VIEW v_viewtest1;
+
+#뷰의 정보 확인
+USE market_db;
+CREATE OR REPLACE VIEW v_viewtest2
+AS
 	SELECT mem_id, mem_name, addr FROM member;
 
-CHECK TABLE naver_db.member;
+DESCRIBE member;
+DESC v_viewtest2;
 
 
+SHOW CREATE TABLE buy;
+SHOW CREATE VIEW v_viewtest2;
 
+#뷰를 통한 데이터 수정/삭제
+UPDATE v_member SET addr = '부산' WHERE mem_id='BLK';
+INSERT INTO v_member(mem_id, mem_name, addr) VALUES('BTS', '방탄소년단', '경기');
+
+CREATE VIEW v_height167
+AS 
+	SELECT * FROM member WHERE height >= 167;
+    
+SELECT * FROM v_height167;
+DELETE FROM v_height167 WHERE height < 167; -- 의미x
+INSERT INTO v_height167 VALUES('TRA', '티아라',6,'서울',NULL,NULL,159,'2005-01-01'); -- 의미 XA
+#WITH CHECK OPTION -> 뷰에 설정된 값의 범위가 벗어나는 값이 입력되지 않도록 함
+ALTER VIEW v_height167
+AS
+	SELECT * FROM member WHERE height >= 167
+		WITH CHECK OPTION ;
+INSERT INTO v_height167(height) VALUES(160);
+
+CHECK TABLE v_height167;
 
 
 
