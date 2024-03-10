c6b499e42e83c5d2719b8dd1145ccfec7e5a3443
ben8169 <ben8169@outlook.kr>
2024-03-10 23:44:35 +0900

    Complete Chapter14

--- Programming Language/C/C_lab/chapter14.c
+++ Programming Language/C/C_lab/chapter14.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 
 int main (void){
-    do 
-    {
-        
-    } while ();
+    int N, answer = 1;
+    printf("Enter N:");
+    scanf("%d",&N);
+
+    while (N > 1){
+        answer *= N;
+        N--;
+    }
+    printf("%d\n",answer);
 
     return 0;
 }
