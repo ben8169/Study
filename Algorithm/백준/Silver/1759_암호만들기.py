7b01e0c314c520052d78cdb50049e0292702042c
ben8169 <ben8169@outlook.kr>
2024-04-06 23:53:14 +0900

    update algorithm

--- Algorithm/백준/Silver/1759_암호만들기.py
+++ Algorithm/백준/Silver/1759_암호만들기.py
@@ -1,36 +1,32 @@
 from itertools import combinations
-L, C = map(int, input().split())
+L, C = list(map(int, input().split()))
 S = input().split()
 S.sort()
 
+
 vowels = ['a','e','i','o','u']
 
-def check(s):
-    vowel_num = 0; consonant_num = 0
-    for ch in s:
-        if ch in vowels:
-            vowel_num += 1
-        else:
-            consonant_num +=1
+# def check(s):
+#     vowel_num = 0; consonant_num = 0
+#     for ch in s:
+#         if ch in vowels:
+#             vowel_num += 1
+#         else:
+#             consonant_num +=1
 
-    if vowel_num >=1 and consonant_num >= 2:
-        return s
-    else:
-        return False
-
-
+#     if vowel_num >=1 and consonant_num >= 2:
+#         return s
+#     else:
+#         return False
 
 
-for i in combinations(S,L):
-    i_checked = check(i)
-    if i_checked:
-        print(''.join(i_checked))
 
 
-# n = C-L+1
-# for i in range(n):
-#     for j in range(i+1,n+1):
-#         for k in range(j+1,n+2):
-#             for l in range(k+1,n+3):
-#                 print(S[i]+S[j]+S[k]+S[l])
+# for i in combinations(S,L):
+#     i_checked = check(i)
+#     if i_checked:
+#         print(''.join(i_checked))
+
+
+
 
