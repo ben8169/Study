d17cbda1a899d3131feb0b5187ed82147d958d6c
ben8169 <ben8169@outlook.kr>
2024-06-20 23:41:34 +0900

    Solve C

---
+++ Programming Language/C/codeup/1035.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void){
+	int a;
+	scanf("%x",&a);
+	printf("%o",a);
+	return 0;
+}	
---
+++ Programming Language/C/codeup/1037.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main(void){
+	int a;
+	scanf("%d",&a);
+	printf("%c",a);
+	return 0;
+
+
+}
+
+
